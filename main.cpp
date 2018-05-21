613f8646d75afc904e135a51478119547499bed0
Chadd Frasier <33643771+ChaddFrasier@users.noreply.github.com>
2018-05-21 12:02:30 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include "List.h"
 
 int main()
@@ -13,23 +12,37 @@ int main()
         cin>> user;
         cout<<"\n"<<endl;
 
+        if(cin.fail()) //cin.fail() checks to see if the value in the cin
+					//stream is the correct type, if not it returns true,
+					//false otherwise.
+		{
+			cin.clear(); //This corrects the stream.
+			cin.ignore(); //This skips the left over stream data.
+			cout << "Please enter an Integer only." << endl;
+		}
         if(user == 1)
         {
             cout<<"Type what data you want to add to the list: "<< endl;
             cin>> user;
             cout<<"\n"<<endl;
-            nodeList.AddNode(user);
-            nodeList.PrintList();
-            user = 0;
+            if(!cin.fail())
+            {
+                nodeList.AddNode(user);
+                nodeList.PrintList();
+                user = 0;
+            }
         }
         else if(user == 2)
         {
             cout<<"Type what you want to remove:  "<< endl;
             cin>> user;
             cout<<"\n"<<endl;
-            nodeList.DeleteNode(user);
-            nodeList.PrintList();
-            user = 0;
+            if(!cin.fail())
+            {
+                nodeList.DeleteNode(user);
+                nodeList.PrintList();
+                user = 0;
+            }
         }
         else if(user == 3)
         {
